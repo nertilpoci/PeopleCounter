d963b9eb4c767a539dbbef4e53ca6c91c6d158f3
NERTIL <Nertilpoci@hotmail.com>
2017-07-03 00:52:09 +0200

    InitialCommit

---
+++ FaceCounter/ValueConverters.cs
@@ -0,0 +1,120 @@
+﻿using Framework.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FaceCounter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+      
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+     
+    }
+    public class ReverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+    public class SourceToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+          if(value!=null && value.ToString()=="Attached")
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+    public class ReverseSourceToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value==null || value.ToString()!="Attached")
+            {
+                return Visibility.Visible;
+            }
+          
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+    
+    public class OutputCombiner : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ObservableCollection<string> collection = new ObservableCollection<string>();
+            for (int i = 0; i < values.Count(); i++)
+            {
+
+                ObservableCollection<string> output = values[i] as ObservableCollection<string>;
+                foreach (string s in output)
+
+                {
+                    collection.Add(s.ToString());
+
+                }
+              
+            }
+            return collection;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
